0efc888a911f86812d68570df0bf2bda2699ab59
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-22 16:22:14 +1100

    main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+//
+//  main.c
+//  function5
+//
+//  Created by Eun Jae Lee on 22/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+//  No argumemt return value
+
+
+#include <stdio.h>
+
+int add();//void
+
+int main() {
+
+    int sum;
+    sum = add();
+    
+    printf("Addition is %d \n",sum);
+    getchar();
+}
+
+int add(){
+    int a,b,add;
+    printf("Enter two numbers: \n");
+    scanf("%d%d", &a,&b);
+    add = a+b;
+    return add;
+}
